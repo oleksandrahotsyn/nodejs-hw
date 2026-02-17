4a5659e2952872b96144c226fb4a6af0f2433f10
Sandra Hotsyn <sandrahotsy@gmail.com>
2026-02-17 09:44:27 +0200

    homework 1 Nodejs

--- src/server.js
+++ src/server.js
@@ -1,4 +1,3 @@
-// src/server.js
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
@@ -7,8 +6,9 @@ import 'dotenv/config';
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+
 app.use(
   pino({
     level: 'info',
@@ -18,56 +18,35 @@ app.use(
         colorize: true,
         translateTime: 'HH:MM:ss',
         ignore: 'pid,hostname',
-        messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
         hideObject: true,
       },
     },
   }),
 );
 
-// Логування часу
-app.use((req, res, next) => {
-    console.log(`Time: ${new Date().toLocaleString()}`);
-    next();
+app.get('/notes', (req, res) => {
+  res.status(200).json({ message: 'Retrieved all notes' });
 });
 
-// Кореневий маршрут
-app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Hello, World!' });
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
 });
 
-// Middleware для парсингу JSON
-app.use(express.json());
-
-app.post('/users', (req, res) => {
-  console.log(req.body); // тепер тіло доступне як JS-обєкт
-  res.status(201).json({ message: 'User created' });
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
 });
 
-// Маршрут для тестування middleware помилки
-app.get('/test-error', (req, res) => {
-    // Штучна помилка для прикладу
-    throw new Error('Something went wrong');
-});
-
-// Middleware 404 (після всіх маршрутів)
 app.use((req, res) => {
-    res.status(404).json({ message: 'Route not found' });
+  res.status(404).json({ message: 'Route not found' });
 });
 
-// Middleware для обробки помилок (останнє)
 app.use((err, req, res, next) => {
-  console.error(err);
-
-  const isProd = process.env.NODE_ENV === "production";
-
-  res.status(500).json({
-    message: isProd
-      ? "Something went wrong. Please try again later."
-      : err.message,
-  });
+  res.status(500).json({ message: err.message });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
