2431fd1d527d27698a81e4582806fcbf2eb2de61
Sandra Hotsyn <sandrahotsy@gmail.com>
2026-02-17 15:59:52 +0200

    install http-errors

---
+++ src/controllers/studentsController.js
@@ -0,0 +1,19 @@
+import { Student } from '../models/students.js';
+
+// Отримати список усіх студентів
+export const getStudents = async (req, res) => {
+  const students = await Student.find();
+  res.status(200).json(students);
+};
+
+// Отримати одного студента за id
+export const getStudentById = async (req, res) => {
+  const { studentId } = req.params;
+  const student = await Student.findById(studentId);
+
+  if (!student) {
+    throw new Error('Student not found');
+  }
+
+  res.status(200).json(student);
+};
