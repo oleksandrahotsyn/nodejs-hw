94af344cd6a7aea84a97d1826fe358a6eb12226d
Sandra Hotsyn <sandrahotsy@gmail.com>
2026-02-18 19:37:48 +0200

    add operation with students

--- src/controllers/studentsController.js
+++ src/controllers/studentsController.js
@@ -1,4 +1,5 @@
 import { Student } from '../models/students.js';
+import createHttpError from 'http-errors';
 
 // Отримати список усіх студентів
 export const getStudents = async (req, res) => {
@@ -12,7 +13,41 @@ export const getStudentById = async (req, res) => {
   const student = await Student.findById(studentId);
 
   if (!student) {
-    throw new Error('Student not found');
+    throw createHttpError(404, 'Student not found');
+  }
+
+  res.status(200).json(student);
+};
+
+export const createStudent = async (req, res) => {
+  const student = await Student.create(req.body);
+  res.status(201).json(student);
+};
+
+export const deleteStudent = async (req, res) => {
+  const { studentId } = req.params;
+  const student = await Student.findOneAndDelete({
+    _id: studentId,
+  });
+
+  if (!student) {
+    throw createHttpError(404, 'Student not found');
+  }
+
+  res.status(200).json(student);
+};
+
+export const updateStudent = async (req, res) => {
+  const { studentId } = req.params;
+
+  const student = await Student.findOneAndUpdate(
+    { _id: studentId }, // Шукаємо по id
+    req.body,
+    { new: true }, // повертаємо оновлений документ
+  );
+
+  if (!student) {
+    throw createHttpError(404, 'Student not found');
   }
 
   res.status(200).json(student);
