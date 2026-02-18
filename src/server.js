b9936755f59e5f9478ce98b52921fbf51e8ed94b
Sandra Hotsyn <sandrahotsy@gmail.com>
2026-02-18 22:22:01 +0200

    fix

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
 
-import { connectMongoDB } from './db/conectMongoDB.js';
+import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
