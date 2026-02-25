0ecfb04fb434aa59e9991bdf994aa2e2394ef2d1
Sandra Hotsyn <sandrahotsy@gmail.com>
2026-02-25 15:45:38 +0200

    add auth/login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -22,3 +22,21 @@ export const registerUser = async (req, res) => {
   // Відправляємо дані користувача (без пароля) у відповіді
   res.status(201).json(newUser);
 };
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  // Перевіряємо чи користувач з такою поштою існує
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw createHttpError(401, 'Invalid credentials');
+  }
+
+  // Порівнюємо хеші паролів
+  const isValidPassword = await bcrypt.compare(password, user.password);
+  if (!isValidPassword) {
+    throw createHttpError(401, 'Invalid credentials');
+  }
+
+  res.status(200).json(user);
+};
