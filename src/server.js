af5147a8536d6fe7bf750e25620af04c2e9c170d
Sandra Hotsyn <sandrahotsy@gmail.com>
2026-02-25 15:15:24 +0200

    add auth

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
+import authRoutes from './routes/authRoutes.js';
 import notesRoutes from './routes/notesRoutes.js';
 
 const app = express();
@@ -15,6 +16,7 @@ const PORT = process.env.PORT ?? 3000;
 app.use(logger);
 app.use(express.json());
 app.use(cors());
+app.use(authRoutes);
 
 app.use(notesRoutes);
 
