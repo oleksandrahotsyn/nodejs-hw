3d043f8ab798c0d58bac9083c79bec66498ef40f
Sandra Hotsyn <sandrahotsy@gmail.com>
2026-02-26 21:33:20 +0200

    add session

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,42 +1,45 @@
 import bcrypt from 'bcrypt';
 import createHttpError from 'http-errors';
 import { User } from '../models/user.js';
+import { createSession } from '../services/auth.js';
+import { Session } from "../models/session.js";
+
 
 export const registerUser = async (req, res) => {
-  const { email, password } = req.body;
+const { email, password } = req.body;
+const existingUser = await User.findOne({ email });
+if (existingUser) {
+throw createHttpError(400, 'Email in use');
+}
 
-  const existingUser = await User.findOne({ email });
-  if (existingUser) {
-    throw createHttpError(400, 'Email in use');
-  }
+const hashedPassword = await bcrypt.hash(password, 10);
 
-  // Хешуємо пароль
-  const hashedPassword = await bcrypt.hash(password, 10);
+const newUser = await User.create({
+email,
+password: hashedPassword,
+});
 
-  // Створюємо користувача
-  const newUser = await User.create({
-    email,
-    password: hashedPassword,
-  });
-
-  // Відправляємо дані користувача (без пароля) у відповіді
-  res.status(201).json(newUser);
+// Створюємо нову сесію
+const newSession = await createSession(newUser._id);
+res.status(201).json(newUser);
 };
 
 export const loginUser = async (req, res) => {
-  const { email, password } = req.body;
+const { email, password } = req.body;
+const user = await User.findOne({ email });
+ if (!user) {
+throw createHttpError(401, 'Invalid credentials');
+}
 
-  // Перевіряємо чи користувач з такою поштою існує
-  const user = await User.findOne({ email });
-  if (!user) {
-    throw createHttpError(401, 'Invalid credentials');
-  }
+const isValidPassword = await bcrypt.compare(password, user.password);
+ if (!isValidPassword) {
+ throw createHttpError(401, 'Invalid credentials');
+ }
 
-  // Порівнюємо хеші паролів
-  const isValidPassword = await bcrypt.compare(password, user.password);
-  if (!isValidPassword) {
-    throw createHttpError(401, 'Invalid credentials');
-  }
+ // Видаляємо стару сесію користувача
+ await Session.deleteOne({ userId: user._id });
 
-  res.status(200).json(user);
+ // Створюємо нову сесію
+ const newSession = await createSession(user._id);
+ res.status(200).json(user);
 };
