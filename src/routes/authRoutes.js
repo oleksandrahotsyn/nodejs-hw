37eeb13710c8037acaeef0d0413432bae2ea0a19
Sandra Hotsyn <sandrahotsy@gmail.com>
2026-02-26 21:56:08 +0200

    add logout

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
-import { loginUser, registerUser } from '../controllers/authController.js';
+import { loginUser, logoutUser, registerUser } from '../controllers/authController.js';
 import {
   loginUserSchema,
   registerUserSchema,
@@ -10,5 +10,6 @@ const router = Router();
 
 router.post('/auth/register', celebrate(registerUserSchema), registerUser);
 router.post('/auth/login', celebrate(loginUserSchema), loginUser);
+router.post('/auth/logout', logoutUser);
 
 export default router;
