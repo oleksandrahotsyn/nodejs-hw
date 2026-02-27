8469ed987a3ef00469c673964395e46661415f8c
Sandra Hotsyn <sandrahotsy@gmail.com>
2026-02-27 10:21:59 +0200

    refresh session

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -67,3 +67,39 @@ export const logoutUser = async (req, res) => {
 
   res.status(204).send();
 };
+
+export const refreshUserSession = async (req, res) => {
+  // 1. Знаходимо поточну сесію за id сесії та рефреш токеном
+  const session = await Session.findOne({
+    _id: req.cookies.sessionId,
+    refreshToken: req.cookies.refreshToken,
+  });
+
+  // 2. Якщо такої сесії нема, повертаємо помилку
+  if (!session) {
+    throw createHttpError(401, 'Session not found');
+  }
+
+  // 3. Якщо сесія існує, перевіряємо валідність рефреш токена
+  const isSessionTokenExpired =
+    new Date() > new Date(session.refreshTokenValidUntil);
+
+  // Якщо термін дії рефреш токена вийшов, повертаємо помилку
+  if (isSessionTokenExpired) {
+    throw createHttpError(401, 'Session token expired');
+  }
+
+  // 4. Якщо всі перевірки пройшли добре, видаляємо поточну сесію
+  await Session.deleteOne({
+    _id: req.cookies.sessionId,
+    refreshToken: req.cookies.refreshToken,
+  });
+
+  // 5. Створюємо нову сесію та додаємо кукі
+  const newSession = await createSession(session.userId);
+  setSessionCookies(res, newSession);
+
+  res.status(200).json({
+    message: 'Session refreshed',
+  });
+};
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
-import { loginUser, logoutUser, registerUser } from '../controllers/authController.js';
+import { loginUser, logoutUser, refreshUserSession, registerUser } from '../controllers/authController.js';
 import {
   loginUserSchema,
   registerUserSchema,
@@ -11,5 +11,6 @@ const router = Router();
 router.post('/auth/register', celebrate(registerUserSchema), registerUser);
 router.post('/auth/login', celebrate(loginUserSchema), loginUser);
 router.post('/auth/logout', logoutUser);
+router.post('/auth/refresh', refreshUserSession);
 
 export default router;
